ef9b73090d4127d51ffb3ce10ee35e30996e318b
Lorenzo Berg <lorenzobegnozzi@hotmail.com>
2024-09-19 19:09:17 -0300

    rota para listar somente os baralhos do jogador que está logado

--- src/deck/deck.controller.ts
+++ src/deck/deck.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { DeckService } from './deck.service';
 
 @Controller('decks')
@@ -6,12 +6,22 @@ export class DeckController {
   constructor(private readonly deckService: DeckService) {}
 
   @Post('create')
-  async createDeck(@Body('commanderName') commanderName: string) {
+  async createDeck(@Body('commanderName') commanderName: string, @Body('userId') userId: string) {
     try {
-      const deck = await this.deckService.createDeck(commanderName);
+      const deck = await this.deckService.createDeck(commanderName, userId);
       return deck;
     } catch (error) {
       throw new Error(`Erro ao criar o deck: ${error.message}`);
+    }
+  }
+
+  @Get()
+  async getDecksByUserId(@Query('userId') userId: string) {
+    try {
+      const decks = await this.deckService.getDecksByUserId(userId);
+      return decks;
+    } catch (error) {
+      throw new Error(`Erro ao buscar decks: ${error.message}`);
     }
   }
 }
--- src/deck/deck.service.ts
+++ src/deck/deck.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { InjectModel } from '@nestjs/mongoose';
@@ -12,59 +12,74 @@ export class DeckService {
     private readonly httpService: HttpService,
     @InjectModel('Deck') private deckModel: Model<Deck>,
     @InjectModel('Card') private cardModel: Model<Card>,
-  ) { }
+  ) {}
 
-  async createDeck(commanderName: string): Promise<Deck> {
+  async createDeck(commanderName: string, userId: string): Promise<Deck> {
     console.log(`Buscando comandante: ${commanderName}`);
 
-    const commanderResponse = await firstValueFrom(
-      this.httpService.get(`https://api.scryfall.com/cards/named?exact=${commanderName}`),
-    );
+    try {
+      const commanderResponse = await firstValueFrom(
+        this.httpService.get(`https://api.scryfall.com/cards/named?exact=${commanderName}`),
+      );
 
-    if (!commanderResponse.data) {
-      throw new NotFoundException(`Comandante ${commanderName} não encontrado.`);
+      if (!commanderResponse.data) {
+        throw new NotFoundException(`Comandante ${commanderName} não encontrado.`);
+      }
+
+      const commander = commanderResponse.data;
+      console.log('Comandante encontrado:', commander);
+
+      if (!commander.color_identity || commander.color_identity.length === 0) {
+        throw new NotFoundException(`Comandante ${commanderName} não possui identidade de cor.`);
+      }
+
+      const colorIdentity = commander.color_identity.join('');
+      console.log('Identidade de cor:', colorIdentity);
+
+      const cardsResponse = await firstValueFrom(
+        this.httpService.get(`https://api.scryfall.com/cards/search?q=color_identity<=${colorIdentity}&unique=cards&order=edhrec`),
+      );
+
+      console.log('Cartas encontradas:', cardsResponse.data);
+
+      const cards = cardsResponse.data.data;
+      const deckCards = cards.slice(0, 99);
+
+      const savedCards = await Promise.all(
+        deckCards.map(async card => {
+          const newCard = new this.cardModel({
+            name: card.name,
+            type: card.type_line,
+            manaCost: card.mana_cost,
+            colors: card.colors,
+            imageUrl: card.image_uris?.normal || '',
+          });
+          return newCard.save();
+        }),
+      );
+
+      const deck = new this.deckModel({
+        commander: commander.name,
+        cards: savedCards.map(savedCard => savedCard._id),
+        user: userId, // Use o userId passado como argumento
+      });
+
+      console.log('Deck criado:', deck);
+
+      return await deck.save();
+    } catch (error) {
+      console.error('Erro ao criar deck:', error);
+      throw new InternalServerErrorException('Erro ao criar deck');
     }
+  }
 
-    const commander = commanderResponse.data;
-    console.log('Comandante encontrado:', commander);
-
-    if (!commander.color_identity || commander.color_identity.length === 0) {
-      throw new NotFoundException(`Comandante ${commanderName} não possui identidade de cor.`);
+  async getDecksByUserId(userId: string): Promise<Deck[]> {
+    try {
+      const decks = await this.deckModel.find({ user: userId }).populate('cards').exec();
+      return decks;
+    } catch (error) {
+      console.error('Erro ao buscar decks pelo ID do usuário:', error);
+      throw new InternalServerErrorException('Erro ao buscar decks');
     }
-
-    const colorIdentity = commander.color_identity.join('');
-    console.log('Identidade de cor:', colorIdentity);
-
-    const cardsResponse = await firstValueFrom(
-      this.httpService.get(`https://api.scryfall.com/cards/search?q=color_identity<=${colorIdentity}&unique=cards&order=edhrec`),
-    );
-
-    console.log('Cartas encontradas:', cardsResponse.data);
-
-    const cards = cardsResponse.data.data;
-    const deckCards = cards.slice(0, 99);
-
-    const savedCards = await Promise.all(
-      deckCards.map(async card => {
-        const newCard = new this.cardModel({
-          name: card.name,
-          type: card.type_line,
-          manaCost: card.mana_cost,
-          colors: card.colors,
-          imageUrl: card.image_uris?.normal || '',
-        });
-        return newCard.save();
-      }),
-    );
-
-   
-    const deck = new this.deckModel({
-      commander: commander.name,
-      cards: savedCards.map(savedCard => savedCard._id),
-    });
-
-    console.log('Deck criado:', deck);
-
-    return deck.save();
   }
 }
